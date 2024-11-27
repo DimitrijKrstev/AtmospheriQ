752ba4516805f6e548402de94d29f339eddfbfde
DimitrijKrstev <antishadowss@gmail.com>
2024-11-27 16:29:59 +0100

    backend init

---
+++ backend/atmospheriq/src/main/java/pulse/eco/atmospheriq/AtmospheriqApplication.java
@@ -0,0 +1,13 @@
+package pulse.eco.atmospheriq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmospheriqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmospheriqApplication.class, args);
+    }
+
+}
---
+++ backend/atmospheriq/src/test/java/pulse/eco/atmospheriq/AtmospheriqApplicationTests.java
@@ -0,0 +1,13 @@
+package pulse.eco.atmospheriq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmospheriqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
